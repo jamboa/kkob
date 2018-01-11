525a8e814d15af0357b2868e8d6805669f40f814
jonghyun.yoo <jonghyun.yoo@samsung.com>
2018-01-11 09:13:33 +0900

    bug fixed

--- kakaoBankHomework/kakaoBankHomework/UIImageViewCached.swift
+++ kakaoBankHomework/kakaoBankHomework/UIImageViewCached.swift
@@ -6,14 +6,16 @@ class UIImageViewCached : UIImageView
     var urlSessionDataTask : URLSessionDataTask?
     
     func applyRounded() -> UIImageViewCached {
-        self.layer.cornerRadius = 15.0
-        self.clipsToBounds = true
+        DispatchQueue.main.async() { [weak self] in
+            self?.layer.cornerRadius = 15.0
+            self?.clipsToBounds = true
+        }
         return self
     }
     
     func downloadImage(url:String) {
-        urlSessionDataTask = Bernoice.shared.getByRemote(url: url){ [weak self]data in
-            DispatchQueue.main.async() {
+        urlSessionDataTask = Bernoice.shared.getByRemote(url: url){ [weak self] data in
+            DispatchQueue.main.async() { [weak self] in
                 self?.contentMode = UIViewContentMode.scaleAspectFill
                 self?.image = UIImage(data: data)
             }
